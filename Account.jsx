185dfd341ed500189900eb5d13ffdac9a272f515
snaky666 <madimoh44@gmail.com>
2025-10-01 17:14:47 +0100

    Add files via upload

---
+++ Account.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Account(){
+  return (
+    <div>
+      <h1 className="text-2xl mb-4">حسابي</h1>
+      <p>حساب وهمي محفوظ محليًا (LocalStorage).</p>
+    </div>
+  )
+}
